4b451465b0d25bfb9771dbf3f751c04d9e132885
stevezhengshiqi <zheng.shi.qi@qq.com>
2023-05-01 14:09:21 -0400

    Initial commit for front end

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,26 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+
+import { rps, rpsls } from './rpsls.js';
+
+let usrMode = null;
+let usrInput = null;
+
+document.querySelector(".play").addEventListener("click", () => {
+    const rpsChecked = document.querySelector("#rps").checked;
+    const rpslsChecked = document.querySelector("#rpsls").checked;
+    const opponentChecked = document.querySelector("#opponent").checked;
+
+    if (rpsCHecked) {
+        usrMode = "rps";
+    } else if (rpslsChecked) {
+        usrMode = "rpsls";
+    }
+
+    if (opponentChecked) {
+        document.querySelector(".inputs").style.display = "block";
+    } else {
+        
+    }
+})
